b2e853287583b0e7542259391564611a391f2c99
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-19 14:30:05 -0800

    updated code to change color of counter to red when > 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
   // --- our code goes here ---
   $('#tweet-text').keyup(function (e) {
     e.preventDefault();
-    $(this).siblings('.counter').text(140 - $(this).val().length);
+    const count = $(this).siblings('.counter');
+    count.text(140 - $(this).val().length);
+
+    if (count.text() < 0) {
+      count.css('color', 'red');
+    }
   });
 });
