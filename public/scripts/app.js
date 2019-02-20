1600187a45b497ab9114f3e9d5ca05d4281bab2b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-19 19:59:56 -0800

    implemented pulling from array and populating tweets dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,20 +6,60 @@
 /* eslint-disable */
 
 $(document).ready(function () {
-  const tweetData = {
-    'user': {
-      'name': 'Newton',
-      'avatars': {
-        'small': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        'regular': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        'large': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png'
+  const tweetData = [{
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      'handle': '@SirIsaac'
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    'content': {
-      'text': 'If I have seen further it is by standing on the shoulders of giants'
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    'created_at': 1461116232227
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  function renderTweets(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    tweets.forEach(element => {
+      $('#tweet-list').prepend(createTweetElement(element));
+    });
   }
 
   function createTweetElement(data) {
@@ -27,7 +67,7 @@ $(document).ready(function () {
     let $tempTweet = $('<article>').addClass('tweet');
     let name = data.user.name;
     let img = data.user.avatars.small;
-    let date = Date(data.created_at).toString();
+    let date = Date(data.created_at).toString().slice(0, 15);
     let handle = data.user.handle;
     let content = data.content.text;
 
@@ -47,16 +87,11 @@ $(document).ready(function () {
 
     $('<footer>').appendTo($tempTweet);
     $('<age>').appendTo($tempTweet.children('footer')).text(date);
-    $('<buttons>').appendTo($tempTweet.children('footer').val('🍕'));
-    $('<buttons>').appendTo($tempTweet.children('footer').val('☃️'));
-    $('<buttons>').appendTo($tempTweet.children('footer').val('🗑'));
+    $('<buttons>').appendTo($tempTweet.children('footer')).text('🍕');
+    $('<buttons>').appendTo($tempTweet.children('footer')).text('☃️');
+    $('<buttons>').appendTo($tempTweet.children('footer')).text('🗑');
 
     return $tempTweet;
   };
-
-  let $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweet-list').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(tweetData);
 });
