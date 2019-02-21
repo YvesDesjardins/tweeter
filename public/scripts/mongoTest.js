f10b04401fc7391d19bc269f4337b4981d3b7fcb
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-21 12:11:17 -0800

    test file completed

--- public/scripts/mongoTest.js
+++ public/scripts/mongoTest.js
@@ -1,4 +1,6 @@
-const [MongoClient] = [require('mongodb').MongoClient]; //eslint-disable-line
+const {
+  MongoClient,
+} = require('mongodb');
 
 const MONGODB_URI = 'mongodb://localhost:27017/tweeter';
 
@@ -12,13 +14,12 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   //     starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> In typical node-callback style, any program
-  //     logic that needs to use the connection needs
-  //     to be invoked from within here.
-  //
-  // Another way to say: this is an 'entry point' for
-  // a database-connected application!
+  db.collection('tweets').find().toArray((err, results) => {
+    if (err) throw err;
 
-  // ==> At the end, we close the connection:
-  db.close();
+    console.log('results array: ', results);
+
+    // This is the end...
+    db.close();
+  });
 });
